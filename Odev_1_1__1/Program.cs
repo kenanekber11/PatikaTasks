1ccd2198eee29a0a86cbf7205b235fbe7b07cf0d
kenanekber11 <79718078+kenanekber11@users.noreply.github.com>
2021-08-20 01:08:29 +0300

    Add files via upload

---
+++ Odev_1_1__1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Odev_1_1__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+            // Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+            // Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+            
+            Console.WriteLine("Kaç adet sayı gireceksiniz? ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] dizi=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("{0}. Sayıyı Giriniz", i+1);
+                dizi[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Girilen çift sayılar ");
+            foreach(int sayilar in dizi){
+                if (sayilar%2==0)
+                {
+                    Console.WriteLine(sayilar);
+                }
+            }
+
+        }
+    }
+}
